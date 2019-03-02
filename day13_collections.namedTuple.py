6616ba47b466025c073a550e27fb216458f6a895
Supraja <supraja.prasad23@gmail.com>
2019-03-02 23:33:01 +0530

    add

---
+++ day13_collections.namedTuple.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from collections import namedtuple
+
+(n, categories) = (int(input()), input().split())
+Grade = namedtuple('Grade', categories)
+marks = [int(Grade._make(input().split()).MARKS) for _ in range(n)]
+print((sum(marks) / len(marks)))
