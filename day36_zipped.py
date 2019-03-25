7c9b0b5d5541f72a2f84565d8d92fdd91bdee48e
Supraja <supraja.prasad23@gmail.com>
2019-03-25 22:09:57 +0530

    add

---
+++ day36_zipped.py
@@ -0,0 +1,3 @@
+N,X=input().split()
+print (*[sum(x)/float(X) for x in zip(*[list(map(float, input().split())) for _ in range(int(X))])],sep='\n')N,X=input().split()
+print (*[sum(x)/float(X) for x in zip(*[list(map(float, input().split())) for _ in range(int(X))])],sep='\n')
